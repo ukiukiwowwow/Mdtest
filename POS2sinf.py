f925063102f07abc7b8c0ea0fa12a74c49883a85
ukiukiwowwow <dreamweapongreen@yahoo.co.jp>
2017-01-24 15:18:21 +0900

    ADDtest
    Newbie

---
+++ POS2sinf.py
@@ -0,0 +1,35 @@
+def sinf(a):
+	g=open(a,"r")
+	linecount=0
+	s=[]
+	sn=[]
+	si=[]
+	o=[]
+	for line in g:
+		linecount+=1
+		if linecount <=5:
+			continue
+		elif linecount == 6:
+			temp=line.split()
+			s.extend(temp)
+			continue
+		elif linecount == 7:
+			temp=line.split()
+			sn.extend(temp)
+			#si=[[0 for i in range(3)] for j in range(int(sn[0]))]
+			#o=[[0 for i in range(3)] for j in range(int(sn[1]))]
+			continue
+		elif linecount == 8 or linecount ==9:
+			continue
+		elif linecount <(10+int(sn[0])):
+			t=line.split()
+			temp=[float(i)*10.32 for i in t[:3]]
+			si.append(temp)
+			continue
+		elif linecount<(34+int(sn[1])):
+			t=line.split()
+			temp=[float(i)*10.32 for i in t[:3]]
+			o.append(temp)
+			continue
+	g.close()
+	return(si,o)
