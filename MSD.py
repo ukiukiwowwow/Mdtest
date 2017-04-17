e2a17d22c111318090dc6dd86c2dfbba4b9c4189
ukiukiwowwow <dreamweapongreen@yahoo.co.jp>
2017-04-17 16:10:52 +0900

    generalizedMSD

--- MSD.py
+++ MSD.py
@@ -2,12 +2,22 @@ import numpy as np
 from copy import deepcopy
 def subMSD():
 	with open("XDATCAR","r") as f,open("cmSiMSD","w") as ps,open("cmOMSD","w") as po,open("cmMSD","w") as cM:
+		L=[[0 for i in range(3)]for j in range(3)]
 		for i in range(7):
 			line=f.readline()
 			if(i==2):
-				L=list(map(float,line.split()))[0]
+				L[0]=list(map(float,line.split()))
+			if(i==3):
+				L[1]=list(map(float,line.split()))
+			if(i==4):
+				L[2]=list(map(float,line.split()))
 			if(i==6):
 				Sinum,Onum=list(map(int,line.split()))
+		print(L)
+		l=[]
+		l.append(np.sqrt(np.dot(L[0],L[0])))
+		l.append(np.sqrt(np.dot(L[1],L[1])))
+		l.append(np.sqrt(np.dot(L[2],L[2])))
 		linecount=0
 		"""
 		print("input lattice parameter")
@@ -61,12 +71,12 @@ def subMSD():
 					cmMSD[2]=(np.sum(Oz,axis=0)+np.sum(Siz,axis=0))/(Sinum+Onum)
 					cM.write(str(time)+" "+str(cmMSD[0])+" "+str(cmMSD[1])+" "+str(cmMSD[2])+"\n")
 				if Sipre.any()==True:
-					Sir=np.dot(Six-cmMSD[0],Six-cmMSD[0])+np.dot(Siy-cmMSD[1],Siy-cmMSD[1])+np.dot(Siz-cmMSD[2],Siz-cmMSD[2])
-					Simsd=1/(Sinum)*Sir*L**2
+					Sir=(Six-cmMSD[0])*L[0]+(Siy-cmMSD[1])*L[1]+(Siz-cmMSD[2])*L[2]
+					Simsd=1/(Sinum)*np.dot(Sir,Sir)
 					ps.write(str(time)+" "+str(Simsd)+"\n")
 				if Opre.any()==True:
-					Or=np.dot(Ox-cmMSD[0],Ox-cmMSD[0])+np.dot(Oy-cmMSD[1],Oy-cmMSD[1])+np.dot(Oz-cmMSD[2],Oz-cmMSD[2])
-					Omsd=1/(Onum)*Or*L**2
+					Or=(Ox-cmMSD[0])*L[0]+(Oy-cmMSD[1])*L[1]+(Oz-cmMSD[2])*L[2]
+					Omsd=1/(Onum)*np.dot(Or,Or)
 					po.write(str(time)+" "+str(Omsd)+"\n")
 				time+=0.001
 				linecount+=1
